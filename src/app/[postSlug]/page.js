c7e18e8d13b72ae7fd634432b8feab38cef352c2
natborks <nathanael@moonsquare.co>
2023-12-27 11:30:56 -0330

    add blog metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { cache } from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 import BlogHero from "@/components/BlogHero";
 
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
+import { BLOG_TITLE } from "@/constants";
+
+export async function generateMetadata({ params }) {
+  const { postSlug } = params;
+
+  const { frontmatter } = await loadBlogPost(postSlug);
+
+  return {
+    title: `${BLOG_TITLE} • ${frontmatter.title}`,
+    description: frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const { postSlug } = params;
